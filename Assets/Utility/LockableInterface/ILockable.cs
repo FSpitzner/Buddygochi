d6d0e38248ef202e54bbe5303235059d53305bf9
FSpitzner <38406798+FSpitzner@users.noreply.github.com>
2019-03-10 14:33:00 +0100

    Utility crap

---
+++ Assets/Utility/LockableInterface/ILockable.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 
+/// </summary>
+public interface ILockable 
+{
+    void Lock(bool locking, Component lockRaiser);
+}
---
+++ Assets/Utility/LockableInterface/Lockable.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 
+/// </summary>
+[System.Serializable]
+public class Lockable : ILockable
+{
+
+    List<Component> locks = new List<Component>();
+    public bool IsLocked = false;
+
+
+
+	public void Lock(bool locking, Component raiser)
+    {
+        if (locking == true)
+        {
+            if (!locks.Contains(raiser))
+            {
+                locks.Add(raiser);
+                IsLocked = true;
+            }
+        }
+        else if (locking == false)
+        {
+            locks.Remove(raiser);
+            if (locks.Count == 0) IsLocked = false;
+        }
+    }
+}
